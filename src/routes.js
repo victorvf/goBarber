0f07ffcd9bb20395b50c64d3ae1548fc73c57f2f
Victor Fontenele <victorvitoria11@hotmail.com>
2020-02-04 16:21:57 -0300

    config database

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import {Router} from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/', async (request, response)=>{
+    const user = await User.create({
+        name:'Victor',
+        email: 'vctor@mail.com',
+        password_hash: '123145'
+    });
+
+    return response.json(user);
+});
 
 export default routes;
