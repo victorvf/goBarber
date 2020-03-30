ba55cc5f77b050f1d6d72173a5104fd3adf8329b
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-30 19:28:56 -0300

    feat: schema validation

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -33,6 +33,13 @@ export const Content = styled.div`
             }
         }
 
+        span {
+            color: rgba(255, 35, 35, 0.6);
+            align-self: flex-start;
+            margin: 0 0 10px;
+            font-weight: bold;
+        }
+
         button {
             margin: 5px 0 0;
             height: 44px;
