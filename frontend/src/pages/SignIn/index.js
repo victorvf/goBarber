15f96387f04a820e5ebf1f2b87c2e30ecb01a3ac
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-30 19:03:14 -0300

    chore: layout signin and signup plus unform

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-    return <h1>SignIn</h1>;
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
+    return (
+        <>
+            <img src={logo} alt="LogoSignIn" />
+
+            <Form onSubmit={handleSubmit}>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="exemplo@email.com"
+                />
+                <Input name="password" type="password" placeholder="senha" />
+
+                <button type="submit">Acessar</button>
+                <Link to="/register">Criar conta gratuita</Link>
+            </Form>
+        </>
+    );
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-    return <h1>SignUp</h1>;
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
+    return (
+        <>
+            <img src={logo} alt="LogoSignIn" />
+
+            <Form onSubmit={handleSubmit}>
+                <Input name="name" type="text" placeholder="Nome completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="exemplo@email.com"
+                />
+                <Input name="password" type="password" placeholder="Senha" />
+
+                <button type="submit">Criar conta</button>
+                <Link to="/">Já tenho conta</Link>
+            </Form>
+        </>
+    );
 }
