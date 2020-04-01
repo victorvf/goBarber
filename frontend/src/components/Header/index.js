d8882df625afdc034cc5aab51b46c3bc331666f8
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-31 23:40:54 -0300

    feat: layout notifications

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Notifications from '../Notifications';
+
+import logo from '~/assets/logo2.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <Content>
+                <nav>
+                    <img src={logo} alt="gobarber" />
+                    <Link to="/dashboard">DASHBOARD</Link>
+                </nav>
+
+                <aside>
+                    <Notifications />
+                    <Profile>
+                        <div>
+                            <strong>Victor Fontenele</strong>
+                            <Link to="/profile">Meu perfil</Link>
+                        </div>
+                        <img
+                            src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                            alt=""
+                        />
+                    </Profile>
+                </aside>
+            </Content>
+        </Container>
+    );
+}
---
+++ frontend/src/components/Notifications/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { MdNotifications } from 'react-icons/md';
+
+import {
+    Container,
+    Badge,
+    Scroll,
+    NotificationList,
+    Notification,
+} from './styles';
+
+export default function Notifications() {
+    return (
+        <Container>
+            <Badge hasUnread>
+                <MdNotifications size={20} color="#7159c1" />
+            </Badge>
+
+            <NotificationList>
+                <Scroll>
+                    <Notification unread>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                </Scroll>
+            </NotificationList>
+        </Container>
+    );
+}
