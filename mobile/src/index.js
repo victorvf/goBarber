97ebc8b53d4ac5fb82b04e3532bc570856859af4
Victor Fontenele <victorvitoria11@hotmail.com>
2020-04-12 01:52:01 -0300

    feat: pages singIn and signUp

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
-    return (
-        <View>
-            <Text>Hello World</Text>
-        </View>
-    );
+    return <Routes />;
 }
